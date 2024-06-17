9f1ab4ac2a00fa1e151b6a5cc0d660edcf011a49
Andriy_Chornobay <andriy.chornobay@gmail.com>
2024-06-17 17:53:35 +0300

    add initial files

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const elements = document.querySelectorAll('#controls button');
+const boxeElement = document.querySelector('#boxes');
+const reader = document.querySelector('#controls input');
+let boxSize = 20; 
+reader.addEventListener('input', handleRead);
+function handleRead(event) {
+  boxSize = 20;
+  return event.currentTarget.value;
+}
+elements[0].addEventListener('click', handleClick);
+function handleClick(event) {
+  boxeElement.innerHTML = ''; 
+  const numberOfBoxes = parseInt(reader.value, 10); 
+  if (numberOfBoxes >= 1 && numberOfBoxes <= 100) {
+    
+    for (let i = 0; i < numberOfBoxes; i++) {
+     
+      const div = document.createElement('div');
+      div.style.backgroundColor = getRandomHexColor();
+      boxSize += 10;
+      div.style.width = `${boxSize}px`;
+      div.style.height = `${boxSize}px`;
+      boxeElement.append(div);
+      
+    }
+  }
+  reader.value = '';
+}
+elements[1].addEventListener('click', deleteColection);
+function deleteColection(event) {
+  boxeElement.innerHTML = '';
+  boxSize = 20;
+}
